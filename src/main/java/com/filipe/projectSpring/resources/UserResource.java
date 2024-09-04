5e82a6149db8f95d77f6983da0c03cda7cb8fc9d
Filipe Samuel <felipepires.p125@gmail.com>
2024-09-03 21:56:00 -0300

    feat: Service layer

--- src/main/java/com/filipe/projectSpring/resources/UserResource.java
+++ src/main/java/com/filipe/projectSpring/resources/UserResource.java
@@ -2,18 +2,34 @@ package com.filipe.projectSpring.resources;
 
 
 import com.filipe.projectSpring.Entities.User;
+import com.filipe.projectSpring.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User("filipe@gmail",1L,"filipe","1234","99858");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
