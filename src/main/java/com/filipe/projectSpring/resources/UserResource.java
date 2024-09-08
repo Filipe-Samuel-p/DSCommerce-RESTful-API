5be37efad7958958b2a4039ab3be4f19719ea821
Filipe Samuel <felipepires.p125@gmail.com>
2024-09-08 20:34:04 -0300

    feat: User insert and HTTP 201

--- src/main/java/com/filipe/projectSpring/resources/UserResource.java
+++ src/main/java/com/filipe/projectSpring/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.filipe.projectSpring.Entities.User;
 import com.filipe.projectSpring.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,6 +29,16 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping // método HTTP para inserção
+    public ResponseEntity<User> insert(@RequestBody  User obj){ //o objeto irá chegar como JSON. Para converter o JSON em um obj para salvar no BD, usa @RequestBody
+        obj = service.insert(obj); // o obj recebe o próprio obj salvo no BD?????
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+        //created retorna um 201, e para isso, precisa mandar um URI.
     }
 
 }
--- src/main/java/com/filipe/projectSpring/services/UserService.java
+++ src/main/java/com/filipe/projectSpring/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
         Optional<User> obj = repository.findById(id);
         return obj.get();
     }
+
+    public User insert(User obj){
+        return repository.save(obj); //o método "save" ja retorna o obj salvo
+    }
 }
