d97d445840dee80377f79ff47e56f818e5b1fa25
Filipe-Samuel-p <felipepires.p125@gmail.com>
2025-01-06 18:16:23 -0300

    feat: first commit

---
+++ src/main/java/com/filipe/projectSpring/Enum/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.filipe.projectSpring.Enum;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT,PAID,SHIPPED,DELIVERED,CANCELED;
+
+    // o Enum é enumerado, então começa do 0, 1, 2...
+    //Quando for atribuir um desses valores em um objeto, basta colocar o numero correspondente do status
+
+}
