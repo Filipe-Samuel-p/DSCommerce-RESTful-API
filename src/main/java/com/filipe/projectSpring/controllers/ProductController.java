a9139f1d87a5602aa8b34b01722d151b3c6cd64b
Filipe-Samuel-p <felipepires.p125@gmail.com>
2025-03-22 22:06:46 -0300

    Atualiza o ProductService e o ProductController

--- src/main/java/com/filipe/projectSpring/controllers/ProductController.java
+++ src/main/java/com/filipe/projectSpring/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.filipe.projectSpring.controllers;
 
+import com.filipe.projectSpring.dto.ProductMinDTO;
 import com.filipe.projectSpring.model.Product;
 import com.filipe.projectSpring.dto.ProductDTO;
 import com.filipe.projectSpring.service.ProductService;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -27,13 +29,14 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(
+    public ResponseEntity<Page<ProductMinDTO>> findAll(
             @RequestParam(name = "name", defaultValue = "") String name,
             Pageable pageable) {
-        Page<ProductDTO> dto = service.findAll(name, pageable);
+        Page<ProductMinDTO> dto = service.findAll(name, pageable);
         return ResponseEntity.ok(dto);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
         dto = service.insert(dto);
@@ -48,6 +51,7 @@ public class ProductController {
         return ResponseEntity.ok(dto);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
